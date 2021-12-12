b921559c9e61f6e723cf15683e10c8066f9914c0
Mike Auty <m@auty.ca>
2021-12-12 07:13:19 -0500

    removed console logs

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,17 +5,12 @@ const useVisualMode = (currentMode) => {
   const [history, setHistory] = useState([mode]);
   
   const transition = (newMode, replace = false) => {
-    // console.log('history before transition:', history)
-    // console.log('incoming from transition:', newMode)
     setMode(newMode)
     
     if (replace === true) {
-      // console.log('inside condition')
       const newHistory = [...history]
       newHistory.pop()
-      // console.log('history after pop', history)
       newHistory.push(newMode)
-      // console.log('history after push', history)
       setHistory(newHistory);
     } else {
       const newHistory = [...history]
@@ -24,20 +19,15 @@ const useVisualMode = (currentMode) => {
       // ALTERNATE METHOD
       // setHistory((prev) => [...prev, newMode]);
     }
-    // console.log('history after transition:', history)
-  
   };
 
   const back = () => {
-    // console.log('history from back function:', history)
-    // console.log('history legnth',history[history.length-1])
     if (history.length > 1) {
       const newHistory = [...history]
       newHistory.pop()
       setHistory(newHistory)
       setMode(newHistory[newHistory.length-1])
     }
-    // console.log('history after pop', history)
   }
 
   const modeObject = {mode, transition, back}
