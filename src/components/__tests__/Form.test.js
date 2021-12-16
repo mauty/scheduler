fa4041ec4f019b268a87f3cb388d0a025bc0a96b
Mike Auty <m@auty.ca>
2021-12-16 14:13:20 -0500

    tested form

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -36,30 +36,6 @@ describe("Form", () => {
     expect(onSave).not.toHaveBeenCalled();
   });
   
-  it("calls onSave function when the name is defined", () => {
-    const onSave = jest.fn();
-    const { getByText, queryByText } = render(<Form interviewers={interviewers} student="Lydia Miller-Jones" onSave={onSave} />);
-    fireEvent.click(getByText("Save"));
-    
-    expect(queryByText(/student name cannot be blank/i)).toBeNull();
-    expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
-  });
-
-  it("submits the name entered by the user", () => {
-    const onSave = jest.fn();
-    const { getByText, getByPlaceholderText } = render(
-      <Form interviewers={interviewers} onSave={onSave} />
-    );
-  
-    const input = getByPlaceholderText("Enter Student Name");
-  
-    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
-    fireEvent.click(getByText("Save"));
-  
-    expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
-  });
 
   it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
